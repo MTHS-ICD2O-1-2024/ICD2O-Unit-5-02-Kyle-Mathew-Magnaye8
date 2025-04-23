ad4ec817a9e196ae576364e7717b22321c6f7db2
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-04-23 00:14:14 +0000

    Saved all work in javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Kyle Matthew Magnaye
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kyle Matthew Magnaye
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+/**
+ * This function checks the user’s choice (positive or negative),
+ * generates a random number, and displays the result.
+ */
+// eslint-disable-next-line no-unused-vars
+function checkNumber() {
+  // input - check which radio button is selected
+  const isPositive = document.getElementById("option-positive").checked
+  let randomNumber
+
+  // process - generate a random number depending on choice
+  if (isPositive) {
+    randomNumber = Math.floor(Math.random() * 9) + 1 // positive number 1–10
+  } else {
+    randomNumber = -1 * (Math.floor(Math.random() * 9) + 1) // negative number -1 to -10
+  }
+
+  // output - show the result
+  document.getElementById("result").innerHTML =
+    "<p>The random number is: <strong>" + randomNumber + "</strong></p>"
+}
